4909fc6c5b354677204f16939b987d53f0a766c7
12435 <124356765@qq.com>
2024-02-01 21:12:31 +0800

    api service support

--- api_service.py
+++ api_service.py
@@ -1,1 +1,40 @@
-# todo
+import socket
+import uvicorn
+from loguru import logger
+from fastapi import Request
+from fastapi.responses import JSONResponse, Response
+from fastapi import FastAPI
+from inference import ChatGPT
+
+app = FastAPI()
+gpt = ChatGPT()
+
+
+def get_host_ip() -> str:
+    """
+    :return: ip
+    """
+    s = None
+    ip = None
+    try:
+        s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+        s.connect(('8.8.8.8', 80))
+        ip = s.getsockname()[0]
+    except Exception as e:
+        logger.info(e)
+    finally:
+        s.close()
+        return ip
+
+
+@app.post("/v1/completions")
+async def completions(request: Request) -> Response:
+    request_dict = await request.json()
+    prompt = request_dict.pop("prompt")
+
+    answer = gpt.chat(prompt)
+    return JSONResponse(answer)
+
+
+if __name__ == "__main__":
+    uvicorn.run(app, host=get_host_ip(), port=8000)
--- api_service_usage.py
+++ api_service_usage.py
@@ -1,1 +1,20 @@
-# todo
+import requests
+import json
+import time
+
+if __name__ == "__main__":
+
+    url = "http://0.0.0.0/v1/completions"
+
+    headers = {"Content-Type": "application/json"}
+
+    prompt = ''' 
+    写一个斐波那契额的算法demo？
+    '''
+    data = {"prompt": prompt}
+
+    start_time = time.time()
+    response = requests.post(url, headers=headers, data=json.dumps(data))
+    end_time = time.time()
+    print(response.text)
+
